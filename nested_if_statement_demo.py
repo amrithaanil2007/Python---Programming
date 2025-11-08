89d57003ad34c699d8c8d5b403e6fa0af0d3ff55
amrithaanil2007 <amrithaanilkumar5@gmail.com>
2025-11-08 16:22:23 +0530

    Add files via upload

---
+++ nested_if_statement_demo.py
@@ -0,0 +1,17 @@
+
+    if has_id:
+        print("you are eligible to vote")
+    else:
+        print("you need an id to vote")
+else:
+    print("You must be atleast 18 to vote")
+
+score = 85
+if score >= 60:
+    if score >= 80:
+        print("you passed with distinction")
+    else:
+        print("you passed.")
+else:
+    print("You failed the exam")
+
